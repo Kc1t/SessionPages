81917bbdf965b716f5d9a1c73c62eb0b546835e8
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2022-10-25 16:07:09 -0300

    inpt

--- atv/index.php
+++ atv/index.php
@@ -1,15 +1,32 @@
+<?php
+
+session_start();
+$conn = mysqli_connect('localhost', 'root', '', 'db_school') or die ('Unable To Connect');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/tailwind.css">
+    <link rel="stylesheet" href="public/css/tailwind.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Atv</title>
 </head>
 <body>
 
- <?php echo "<p>Olá Mundo</p>"; ?>
+    <h2>Please Login</h2>
+
+    <div> 
+        <form action="index.php" method="post">
+            <input type="text" name="Username" placeholder="Username" required = ""><br>
+            <input type="password" name="Pass" placeholder="Password" required = ""><br>
+            <input type="submit" name="login" value="Login" required = "">
+            </form>
+    </div>
 
 </body>
 </html>
+
